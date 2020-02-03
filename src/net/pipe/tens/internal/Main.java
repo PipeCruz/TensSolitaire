938a15435521c97a9158a0ad492e503d94cd44d2
Pipe Cruz <43627567+PipeCruz@users.noreply.github.com>
2020-02-02 20:19:28 -0500

    Final Commit
    Signed-off-by: Pipe Cruz <43627567+PipeCruz@users.noreply.github.com>

--- src/net/pipe/tens/internal/Main.java
+++ src/net/pipe/tens/internal/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 750, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
+        primaryStage.show();//fixme below
         primaryStage.getIcons().add(new Image("/net/pipe/tens/files/css/icon.png"));
         primaryStage.setOnCloseRequest(e -> {
             Platform.exit();
